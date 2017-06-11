690dacf21adce4dc6341878a5e5cf3164c6f31d8
Ilham Salleh <ilhamsalleh91@gmail.com>
2017-06-11 09:36:15 +0800

    Initial commit

---
+++ CTTZip.java
@@ -0,0 +1,266 @@
+package xmlTools;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.xpath.XPathExpressionException;
+
+import org.xml.sax.SAXException;
+
+/**
+ * 
+ * Unzips CTTs
+ *
+ */
+class CTTZip{
+	final static int BUFFER_SIZE = 4096;
+	private HashMap<String, String> args = new HashMap<String, String>();
+	
+	public CTTZip() {
+		args.put("modify", "");
+	}
+	
+	/**
+	 * Set arguments for the CopyAndModifyXML class, e.g. instructions for modification
+	 */
+	public void setArgs(String key, String value){
+		args.put(key, value);
+	}
+	
+	public void startCopying() throws IOException, URISyntaxException, XPathExpressionException, ParserConfigurationException, SAXException, TransformerException{
+		rezipFile(processingXMLs(unzipFile(), args));
+	}
+	
+	
+	/**
+	 * Processes XMLs by passing each single XML to XMLModAndCopy
+	 * @param ruleMap
+	 * @return
+	 * @throws ParserConfigurationException 
+	 * @throws TransformerException 
+	 * @throws IOException 
+	 * @throws SAXException 
+	 * @throws XPathExpressionException 
+	 */
+	private HashMap<Path, ByteArrayOutputStream> processingXMLs(HashMap<Path, ByteArrayOutputStream> ruleMap, 
+			HashMap<String, String> args) throws ParserConfigurationException, XPathExpressionException, SAXException, IOException, TransformerException{
+		//modified rule map	
+		HashMap<Path, ByteArrayOutputStream> modRuleArray = new HashMap<Path, ByteArrayOutputStream>();
+				
+		for (HashMap.Entry<Path, ByteArrayOutputStream> ruleOut : ruleMap.entrySet()){
+			//convert to input streams
+			ByteArrayInputStream ruleIn = new ByteArrayInputStream(ruleOut.getValue().toByteArray());
+			//creates a new instance of the copying XML class to copy and modify each XML
+			XMLModAndCopy modAndCopyXML = new XMLModAndCopy(args);
+			System.out.println("Copying " + ruleOut.getKey() + "...");
+		    modRuleArray.put(ruleOut.getKey(), modAndCopyXML.copyXML(ruleIn));
+		} 
+			
+	return modRuleArray;
+	}
+
+	
+	/**
+	 * unzips a zipped file to get accounting rules
+	 * @return
+	 * @throws IOException 
+	 */
+	private HashMap<Path, ByteArrayOutputStream> unzipFile() throws IOException{
+		HashMap<Path, ByteArrayOutputStream> ruleArray = new HashMap<Path, ByteArrayOutputStream>();
+			File inputZip = new File(args.get("input"));
+			ZipInputStream zipIn = null;
+			if (!inputZip.exists() || !inputZip.canRead()){
+				throw new FileNotFoundException("Cannot find/read input file " + args.get("input") + ".");
+			}
+			else{
+				zipIn = new ZipInputStream(new FileInputStream(args.get("input")));
+			}
+			
+			ZipEntry entry = null;
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			while ((entry = zipIn.getNextEntry()) != null) {
+				//System.out.println(entry.getName());
+			    if (entry.getName().equals("static_data/CM.201.zip")) {
+			    	BufferedOutputStream bos = new BufferedOutputStream(out);
+			        byte[] buffer = new byte[BUFFER_SIZE];
+			        int len;
+			        while ((len = zipIn.read(buffer)) != -1) {
+			            bos.write(buffer, 0, len);
+			        }
+			        bos.close();
+			        break;
+			    }
+			}
+			zipIn.close();
+			
+			//open the cm201zip file and add all accounting rules to a Hashmap of filenames and file contents
+			
+			ZipInputStream cm201zip = new ZipInputStream(new ByteArrayInputStream(out.toByteArray()));
+			entry = null;
+			while ((entry = cm201zip.getNextEntry()) != null) {
+				Path entryPath = Paths.get(entry.getName());
+				if (entryPath.getParent() != null && entryPath.getParent().toString().equals("mx\\accounting\\Rule")) {
+					ByteArrayOutputStream out2 = new ByteArrayOutputStream();
+					byte[] buffer = new byte[BUFFER_SIZE];
+		            int len;
+		            while ((len = cm201zip.read(buffer)) != -1) {
+		                out2.write(buffer, 0, len);
+		            }
+		            out2.close();
+		            ruleArray.put(entryPath, out2);
+				}
+			}
+		return ruleArray;
+	}
+	
+	/**
+	 * recreates the Zip
+	 * @param toBeZippedFiles
+	 * @throws IOException
+	 * @throws URISyntaxException
+	 */
+	private void rezipFile(HashMap<Path, ByteArrayOutputStream> toBeZippedFiles) throws IOException, URISyntaxException{
+		final int BUFFER_SIZE = 4096;
+		ZipInputStream zin = null;
+		//test zip
+		File outputZip = new File(args.get("output"));
+		if (outputZip.exists()){
+			outputZip.delete();
+		}
+		Path outputZipPath = Paths.get(outputZip.getPath());
+		Map<String, String> env = new HashMap<String, String>();
+	    env.put("create", String.valueOf(Files.notExists(outputZipPath)));
+	    // use a Zip filesystem URI
+	    URI fileUri = outputZipPath.toUri(); // here
+	    URI zipUri = new URI("jar:" + fileUri.getScheme(), fileUri.getPath(), null);
+	    //System.out.println(zipUri);
+	    try (FileSystem zipfs = FileSystems.newFileSystem(zipUri, env)) {
+	    	System.out.println("Recreating the zip with the copied rules...");
+			if (!(new File(args.get("input"))).exists() || !(new File(args.get("input"))).canRead()){
+				throw new FileNotFoundException("Cannot find/read input file " + args.get("input") + ".");
+			}
+			else if(((new File(args.get("output"))).exists() && !(new File(args.get("output"))).canWrite())){
+				throw new FileNotFoundException("Cannot write to output file " + args.get("output") + ".");
+			}
+			else{
+				zin = new ZipInputStream(new FileInputStream(args.get("input")));
+			}
+			ByteArrayOutputStream cm201zip = new ByteArrayOutputStream();
+			//open outer zip
+			
+			ZipEntry entry = null;
+			while ((entry = zin.getNextEntry()) != null) {
+				boolean toBeDeleted = false;
+				Path entryPath = Paths.get(entry.getName());
+				//copy everything but cm201
+				if (entryPath.getFileName().toString().equals("CM.201.zip")) {
+					toBeDeleted = true;
+			    	BufferedOutputStream bos = new BufferedOutputStream(cm201zip);
+			        byte[] buffer = new byte[BUFFER_SIZE];
+			        int len;
+			        while ((len = zin.read(buffer)) != -1) {
+			            bos.write(buffer, 0, len);
+			        }
+			        bos.close();
+				}
+				if(!toBeDeleted){
+					addToTempFolder(zipfs, "/", zin, entryPath);
+		        }
+			}
+			zin.closeEntry();
+			zin.close();
+			
+			//change cm201zip, add the new XMLs
+			//test inner zip
+			//Path cm201Path = zipfs.getPath("/static_data/CM.201.zip");
+			//Files.createDirectories(cm201Path.getParent());
+			//Files.createFile(cm201Path);
+			File cm201Zip = new File("tempcm201.zip");
+			if (cm201Zip.exists()){
+				cm201Zip.delete();
+			}
+			Path cm201ZipPath = Paths.get(cm201Zip.getPath());
+		    // use a Zip filesystem URI
+		    URI cm201Uri = cm201ZipPath.toUri(); // here
+		    URI cm201zipUri = new URI("jar:" + cm201Uri.getScheme(), cm201Uri.getPath(), null);
+			Map<String, String> env2 = new HashMap<String, String>();
+		    env2.put("create", String.valueOf(Files.notExists(cm201ZipPath)));
+		    
+		    try (FileSystem cm201fs = FileSystems.newFileSystem(cm201zipUri,env2)) {
+			
+				ZipInputStream cm201zipInStream = new ZipInputStream(new ByteArrayInputStream(cm201zip.toByteArray()));
+				entry = null;
+	
+				while ((entry = cm201zipInStream.getNextEntry()) != null) {
+					boolean toBeDeleted = false;
+					Path entryPath = Paths.get(entry.getName());
+					//replace existing rules in zip
+					if (entryPath.getParent() != null && entryPath.getParent().toString().equals("mx\\accounting\\Rule")) {
+						toBeDeleted = true;
+					}
+					if(!toBeDeleted){		
+						addToTempFolder(cm201fs, "/", cm201zipInStream, entryPath);
+					}
+				}
+				
+				cm201zipInStream.close();
+				
+				for (HashMap.Entry<Path, ByteArrayOutputStream> modifiedXML : toBeZippedFiles.entrySet()){
+					ByteArrayInputStream ruleIn = new ByteArrayInputStream(modifiedXML.getValue().toByteArray());
+					addToTempFolder(cm201fs, "/", ruleIn, modifiedXML.getKey());
+				}
+		    }
+		    //move cm201 back into the outer zip
+		    Path cm201Path = zipfs.getPath("/static_data/CM.201.zip");
+			Files.createDirectories(cm201Path.getParent());
+		    Files.copy(cm201ZipPath, cm201Path);
+		    cm201Zip.delete();
+	    }
+		//end test zip
+		
+
+	} 	    
+	
+	
+	
+	/**
+	 * Helper method to add the files to a zip FS
+	 * @param zipfs
+	 * @param directoryPath
+	 * @param inputStream
+	 * @param entryPath
+	 * @throws IOException
+	 */
+	private void addToTempFolder(FileSystem zipfs, String directoryPath, InputStream inputStream, Path entryPath) throws IOException{
+		Path internalTargetPath = zipfs.getPath(directoryPath + "/" + entryPath.toString());
+		Files.createDirectories(internalTargetPath.getParent());
+		OutputStream fileOut = Files.newOutputStream(internalTargetPath);
+		byte[] buffer = new byte[BUFFER_SIZE];
+        int len;
+        while ((len = inputStream.read(buffer)) != -1) {
+            fileOut.write(buffer, 0, len);
+        }
+        fileOut.close();
+	}
+	
+}
---
+++ XMLModAndCopy.java
@@ -0,0 +1,201 @@
+package xmlTools;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.HashMap;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+/**
+ * Modifies and copies a single XML file
+ */
+class XMLModAndCopy{
+	private DocumentBuilderFactory factory;
+	private DocumentBuilder builder;
+	private HashMap<String, String> args;
+	private XPath xPath;
+	
+	//no-arg constructor
+	XMLModAndCopy() throws ParserConfigurationException{
+		this(new HashMap<String, String>());
+	}
+	
+	
+	XMLModAndCopy(HashMap<String, String> args) throws ParserConfigurationException{
+		this.factory = DocumentBuilderFactory.newInstance();
+		this.builder = factory.newDocumentBuilder();
+		this.xPath =  XPathFactory.newInstance().newXPath();
+		this.args = args;
+	}
+	
+
+	/**
+	 * Copies a single XML
+	 * @param ruleMap
+	 * @return 
+	 * @throws IOException 
+	 * @throws SAXException 
+	 * @throws XPathExpressionException 
+	 * @throws TransformerException 
+	 */
+	public ByteArrayOutputStream copyXML(InputStream ruleIn) throws SAXException, IOException, XPathExpressionException, TransformerException{
+		ByteArrayOutputStream byteOutput = new ByteArrayOutputStream();
+		//convert to input streams
+		Document doc = builder.parse(ruleIn);
+		String newDocXmlDecl = "<MxML version=\"1-1\"></MxML>";
+		InputSource newDocSource = new InputSource(new StringReader(newDocXmlDecl));
+		Document newDoc = builder.parse(newDocSource);
+		//copy XML structure
+		
+		String xPathExp = "MxML";
+		NodeList nodeList = (NodeList) xPath.compile(xPathExp).evaluate(doc, XPathConstants.NODESET);
+		Node mxmlNode = nodeList.item(0);
+		NodeList level2Nodes = mxmlNode.getChildNodes();
+		for (int i = 0; i < level2Nodes.getLength(); i++) {
+			Node level2Node = level2Nodes.item(i);
+			Node newLevel2Node = newDoc.importNode(level2Node, true);
+			newDoc.getDocumentElement().appendChild(newLevel2Node);		
+		}
+		
+		//call ModifyXML to modify the new XML document
+		modifyXML(newDoc, args);
+					
+		// write the content into Xml file
+		newDoc.setXmlStandalone(true);
+	    DOMSource source = new DOMSource(newDoc);
+	    Writer writer = new OutputStreamWriter(byteOutput);
+	    StringWriter strWriter = new StringWriter();
+	    //add a custom xml declaration
+	    String customXML = "<?xml version=\"1.0\"?>\n";
+	    StreamResult result = new StreamResult(strWriter);
+
+	    TransformerFactory transformerFactory = TransformerFactory.newInstance();
+	    Transformer transformer = transformerFactory.newTransformer();
+	    transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+	    transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+	    //remove normal XML declaration
+	    transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+	    transformer.transform(source, result);
+	    writer.write(customXML + strWriter.toString());
+	    strWriter.close();
+	    writer.close();
+		return byteOutput;
+	}
+	
+	/**
+	 * Modifies a single XML
+	 * @throws XPathExpressionException to CopyXML
+	 * @throws IOException to CopyXML
+	 * @throws SAXException to CopyXML
+	 */
+	private void modifyXML(Document newDoc, HashMap<String, String> args) throws SAXException, IOException, XPathExpressionException{
+		Document docWithAddedNodes = null;
+		if (args.get("modify") != null){
+			switch (args.get("modify")){
+			case "Tick Additional Entity":
+				removeUDFs(newDoc);
+				addUDF(newDoc, docWithAddedNodes);
+				break;
+			case "Tick A Different Entity":
+				changeUDF(newDoc);
+				break;
+			default:
+				System.out.print("An invalid option was entered.\n");
+				System.out.println("Nothing has been done to this XML.");
+			}
+		}
+	}
+	
+	private void changeUDF(Document doc) throws XPathExpressionException{
+		String xPathExp = "MxML/mxAccountingIRULESet/mxAccountingIRULE/userDefinedField[fieldValue=\""
+				+ args.get("fromEntity") + "\" and fieldLabel=\"TrnEntity\"]/fieldValue";
+		NodeList nodeList = (NodeList) xPath.compile(xPathExp).evaluate(doc, XPathConstants.NODESET);
+		if (nodeList.getLength() == 0){
+			System.out.println("No nodes to replace.");
+		}				
+		for (int i = 0; i < nodeList.getLength(); i++) {
+			Node fieldValueNode = nodeList.item(i);
+			for (int j = 0; j < fieldValueNode.getChildNodes().getLength(); j++){
+				Node childNode = fieldValueNode.getChildNodes().item(i);
+				if (childNode.getNodeType() == Node.TEXT_NODE) {
+					childNode.setNodeValue(args.get("toEntity"));
+				}
+			}
+		}
+	}
+	
+	private void removeUDFs(Document doc) throws XPathExpressionException{
+		String xPathExp = "MxML/mxAccountingIRULESet/mxAccountingIRULE/userDefinedField";
+		//remove user defined fields
+		NodeList nodeList = (NodeList) xPath.compile(xPathExp).evaluate(doc, XPathConstants.NODESET);
+		for (int i = 0; i < nodeList.getLength(); i++) {
+			Node udfNode = nodeList.item(i);
+			udfNode.getParentNode().removeChild(udfNode);
+		}  
+	}
+		
+	
+	private void addUDF(Document newDoc, Document docWithAddedNodes) throws XPathExpressionException, SAXException, IOException{
+		InputSource addUDFSource = new InputSource(new StringReader(addAdditionalEntity(args.get("entity"))));
+		Document addUDFDoc = builder.parse(addUDFSource);
+
+
+		Node toBeCopiedEntity = newDoc.importNode(addUDFDoc.getDocumentElement(), true);
+		String xPathExp = "MxML/mxAccountingIRULESet/mxAccountingIRULE";
+		Node mxAccountingIRULENode = ((NodeList) xPath.compile(xPathExp).evaluate(newDoc, XPathConstants.NODESET)).item(0);
+		mxAccountingIRULENode.appendChild(toBeCopiedEntity);
+	}
+	
+	private String addAdditionalEntity(String entity){
+		//add the additional entity into the document
+		
+		String additionalEntity = ""
+				+ "<userDefinedField>"
+				+ "<fieldLabel>FilterDetails</fieldLabel>"
+				+ "<mxAccountingIRULE_FilterDetails>"
+				+ "<mxAccountingIRULE_FILTER_DETAIL>"
+				+ "<businessObjectId mefClass=\"mxAccountingIRULE_FILTER_DETAIL\">"
+				+ "<primarySystem>MX</primarySystem>"
+				+ "</businessObjectId>"
+				+ "<userDefinedField>"
+				+ "<fieldLabel>FieldType</fieldLabel>"
+				+ "<fieldValue>0</fieldValue>"
+				+ "<fieldType>integer</fieldType>"
+				+ "</userDefinedField>"
+				+ "<userDefinedField>"
+				+ "<fieldLabel>FieldValue</fieldLabel>"
+				+ "<fieldValue>" + entity + "</fieldValue>"
+				+ "<fieldType>character</fieldType>"
+				+ "</userDefinedField>"
+				+ "</mxAccountingIRULE_FILTER_DETAIL>"
+				+ "</mxAccountingIRULE_FilterDetails>"
+				+ "</userDefinedField>";
+		return additionalEntity;
+
+	}
+	
+}
+
